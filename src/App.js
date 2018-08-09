97f60621396eec569edd43351e618e4adfc20a65
Arran Gravestock <arran@gravestock.net>
2018-08-09 15:40:34 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Nav  from './Nav.js';
-import {BrowserRouter as Router} from 'react-router-dom'
+import {HashRouter as Router} from 'react-router-dom'
 
 class App extends Component {
   render() {
